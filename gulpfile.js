5c27ae128e0696dc9119b3029e13286656ae6c4c
starrier <starrier@starrier.org>
2021-01-27 21:13:42 +0800

    add budget.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var uglify = require('gulp-uglify-es').default;
 var htmlmin = require('gulp-htmlmin');
 
 // 代理
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         reloadDebounce: 500,
         proxy: 'localhost:4000'
@@ -16,15 +16,15 @@ gulp.task('browser-sync', function() {
 });
 
 // 压缩css
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function () {
     return gulp.src('public/**/*.css')
-        .pipe(minifycss().on('error', function(e) {
+        .pipe(minifycss().on('error', function (e) {
             console.log(e)
         }))
         .pipe(gulp.dest('public'));
 });
 // 压缩html
-gulp.task('minify-html', function() {
+gulp.task('minify-html', function () {
     var option = {
         removeComments: true,
         minifyJS: true,
@@ -36,13 +36,13 @@ gulp.task('minify-html', function() {
         .pipe(gulp.dest('public'));
 });
 // 压缩js
-gulp.task('minify-js', function() {
+gulp.task('minify-js', function () {
     return gulp.src('public/js/**/*.js')
-        .pipe(uglify().on('error', function(e){
+        .pipe(uglify().on('error', function (e) {
             console.log(e)
         }))
         .pipe(gulp.dest('public/js'));
 });
 
 //gulp.task('default',gulp.parallel('browser-sync'));
-gulp.task('default', gulp.parallel('minify-css','minify-js'));
+gulp.task('default', gulp.parallel('minify-css', 'minify-js'));
